1a8e1074c559d3e40b31c6a14c3bd73441f52e13
Pwiley24 <97415028+Pwiley24@users.noreply.github.com>
2022-01-12 12:23:49 -0800

    Update RobotP.java

--- robot_p/RobotP.java
+++ robot_p/RobotP.java
@@ -1,61 +1,77 @@
 package robot_p;
-import robocode.AdvancedRobot;
+import robocode.*;
 import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
 /**
  * RobotP - a robot by Paige Wiley
- * This robot will be controlled by the user.
+ * A Robot that zig zags across the field.
+ * Scans the battle field and predicts where the robots are heading
+ * Fires in those directions
+ * If close enough to another robot, ram.
  */
-public class RobotP extends Robot
+
+
+public class RobotP extends AdvancedRobot
 {
+
+
+	
 	/**
 	 * run: RobotP's default behavior
 	 */
-	 
-	public void onKeyPressed (KeyEvent e){
-		if(e.getKeyCode() == 32) {//If the space bar is pressed fire the bullet
-			fireBullet(//assign a power level);
-	}
-	
-	/*
-	 * Will determine the direction the tank will move in 
-	 * and where the gun is pointed
-	 */
-	public void onMousedMoved (KeyEvent e){
-		int aimX;
-		int aimY;
-		//Redirect where the gun is pointed 
-		aimX = e.getX();
-		aimY = e.getY();
-		turnGunLeft(aimX);
-	}
-	
 	public void run() {
 		// Initialization of the robot should be put here
-
+		setAdjustRadarForGunTurn(true);
+		setAdjustGunForRobotTurn(true);
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-		
+		setColors(Color.white,Color.blue,Color.green); // body,gun,radar
 
 		// Robot main loops
 		
 		while(true) {
-			// 
+			// Replace the next 4 lines with any behavior you would like
+			setTurnRadarLeft(5);
+			setTurnGunLeft(5);
+			setAhead(100);
+			scan();
+			execute();
 			
-			
+			while(getDistanceRemaining() > 0 && getGunTurnRemaining() > 0 && getRadarTurnRemaining() > 0){
+				execute();
+			}
+
+				
 		}
 	}
+	
+	
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
+		
 		fire(1);
+		
+		turnGunLeft(10);
+		turnRadarLeft(10);
+		fire(1);
+		
+		turnGunRight(20);
+		turnRadarRight(20);
+		fire(1);
+		
+
+	}
+	
+	public void onRobotHit(HitRobotEvent e){
+		back(10);
+		turnLeft(50);
 	}
 
 	/**
@@ -63,7 +79,7 @@ public class RobotP extends Robot
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
-		back(10);
+		
 	}
 	
 	/**
@@ -71,10 +87,6 @@ public class RobotP extends Robot
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(20);
-	}
-	
-	
-	
-	
+		turnLeft(45);
+	}	
 }
